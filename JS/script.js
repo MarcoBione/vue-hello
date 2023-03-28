332ffc48bb2eb9d438a8174d7493c1c3fba4102d
MarcoBione <marcobione.work@gmail.com>
2023-03-28 13:10:10 +0200

    Add data to createApp @ script

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,14 @@
 //method createApp from vue (import del metodo per creare la mia app)
 const { createApp } = Vue;
 
+//invoco metodo createApp
+createApp({
+    //dati con classi
+    data(){
+        return {
+            title: 'hello Vue js',
+            image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FFelis_silvestris_catus&psig=AOvVaw2nrHB-3SENTZLZKnlRqCrf&ust=1680084579329000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKjm3_qw_v0CFQAAAAAdAAAAABAI'
+        }
+    }
+
+}).mount('#app'); //appendo il contenuto nel div 'app' principale
